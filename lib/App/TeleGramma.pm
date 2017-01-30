1d06050100468471ed54174a6e2816e42d746971
Justin Hawkins <justin@eatmorecode.com>
2017-01-30 19:47:03 +1030

    Improve documentation, README generation

--- lib/App/TeleGramma.pm
+++ lib/App/TeleGramma.pm
@@ -2,6 +2,63 @@ package App::TeleGramma;
 
 # ABSTRACT: A modular Telegram Bot
 
+=head1 NAME
+
+App::TeleGramma - A modular Telegram Bot
+
+=head1 SYNOPSIS
+
+Install App::TeleGramma and its dependencies
+   $ cpanm App::TeleGramma
+
+The first time run, a basic configuration file is automatically created for you.
+
+   $ telegramma
+   Your new config has been created in /Users/username/.telegramma/telegramma.ini
+
+   Please edit it now and update the Telegram Bot token, then
+   re-run bin/telegramma.
+
+   The configuration will have an entry for each plugin currently available on
+   your system, but disabled.
+
+Edit the config file, adding (at least) the Telegram Bot API key.
+
+Now you can run, first in foreground mode for testing purposes:
+
+   $ telegramma --nodaemon
+
+When it's all good, you'll want to run it as a daemon:
+
+   $ telegramma
+
+You can monitor the status of the running process, and shut it down.
+
+   $ telegramma --status
+
+   $ telegramma --shutdown
+
+=head1 DESCRIPTION
+
+TeleGramma is an easy to use, extensible bot to use with Telegram C<www.telegram.org>.
+
+Its plugin architecture makes it easy to add new modules either from other authors,
+or yourself.
+
+=head1 BUGS
+
+None known.
+
+=head1 AUTHOR
+
+Justin Hawkins C<justin@eatmorecode.com>
+
+=head1 SEE ALSO
+
+L<Telegram::Bot> - the lower level API
+
+=cut
+
 use Mojo::Base 'Telegram::Bot::Brain';
 
 use App::TeleGramma::Config;
