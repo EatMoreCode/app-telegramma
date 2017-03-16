94c58453dc075b64768a4b8ffae3cef1d697e336
Justin Hawkins <justin@eatmorecode.com>
2017-03-16 20:13:12 +1030

    Add new plugin and allow for regexs for commands

--- lib/App/TeleGramma/BotAction/Listen.pm
+++ lib/App/TeleGramma/BotAction/Listen.pm
@@ -16,8 +16,14 @@ sub process_message {
 
   my $cmd = $self->command;
 
-  if ($msg->text && $msg->text =~ /^\Q$cmd\E \b @?/x) {
+  if ($msg->text && ! ref($cmd) && $msg->text =~ /^\Q$cmd\E \b @?/x) {
     my ($body) = ($msg->text =~ /^ \S+ \s+ (.*)$/x);
+    return $self->response->($msg, $body) if defined $body; # return body of command, if it existed
+    return $self->response->($msg);
+  }
+
+  if ($msg->text && ref($cmd) eq 'Regexp' && $msg->text =~ $cmd) {
+    my $body = ($msg->text);
     return $self->response->($msg, $body) if defined $body; # return body of command, if it existed
     return $self->response->($msg);
   }
