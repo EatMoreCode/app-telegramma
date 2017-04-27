79c88ee8435f3c845704396147cf2564e42d1c62
Justin Hawkins <justin@eatmorecode.com>
2017-04-27 15:35:02 +0930

    New plugin for setting timers.

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ App::TeleGramma - A modular Telegram Bot
 
 # VERSION
 
-version 0.08
+version 0.09
 
 # SYNOPSIS
 
---
+++ lib/App/TeleGramma/Plugin/Core/Timer.pm
@@ -0,0 +1,77 @@
+package App::TeleGramma::Plugin::Core::Timer;
+
+# ABSTRACT: TeleGramma plugin to set timers
+
+use Mojo::Base 'App::TeleGramma::Plugin::Base';
+use App::TeleGramma::BotAction::Listen;
+use App::TeleGramma::Constants qw/:const/;
+use Time::Duration::Parse qw/parse_duration/;
+
+sub synopsis {
+  "Set timers for a future reminder"
+}
+
+sub default_config {
+  my $self = shift;
+  return { };
+}
+
+sub register {
+  my $self = shift;
+
+  my $timer_help = App::TeleGramma::BotAction::Listen->new(
+    command  => qr{/timer}i,
+    response => sub { $self->timer_help(@_) }
+  );
+
+  my $timer_set_exact = App::TeleGramma::BotAction::Listen->new(
+    command  => qr{/timer\s(.*)}i,
+    response => sub { $self->timer_set(@_) }
+  );
+
+  return ($timer_set_exact, $timer_help);
+}
+
+sub timer_help {
+  my $self = shift;
+  my $msg  = shift;
+  $self->reply_to($msg, "examples: /timer remind me to weed and feed in 3 hours");
+  return PLUGIN_RESPONDED;
+}
+
+sub timer_set {
+  my $self = shift;
+  my $msg  = shift;
+
+  my $text = $msg->text;
+  my ($request) = ($text =~ m{/timer\s+(.*)}i);
+
+  # remove some common prefixes from the request
+  $request =~ s/^remind me to//;
+  $request =~ s/^remind me//;
+  $request =~ s/^tell me to//;
+
+  # try to parse the thing, starting at the first number
+  my ($timer_text, $duration_text) = ($request =~ /^\s*(.+)\s+in\s+(\d.*)/);
+  if (! $duration_text) {
+    $self->reply_to($msg, "sorry, I can't work out when you mean from '$text'");
+    return PLUGIN_RESPONDED;
+  }
+
+  my $duration = eval { parse_duration($duration_text) };
+  if ($@ || ! $duration) {
+    $self->reply_to($msg, "sorry, I can't work out when you mean from '$duration_text'");
+    return PLUGIN_RESPONDED;
+  }
+
+  Mojo::IOLoop->timer($duration => sub {
+    my $loop = shift;
+    my $message = "Hey " . $msg->from->username . ", this is your reminder to $timer_text";
+    $self->reply_to($msg, $message);
+  });
+
+  $self->reply_to($msg, "Will remind you '$timer_text' in $duration_text");
+  return PLUGIN_RESPONDED;
+}
+
+1;
